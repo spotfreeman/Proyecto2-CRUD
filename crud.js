d59ebe451275ea81eaa40d87fe6a4114af48e6d6
Raul Oyarzun B <r.oyarzun83@gmail.com>
2023-05-10 21:32:31 -0400

    armando plantilla

---
+++ crud.js
@@ -0,0 +1,11 @@
+let documentos = new Array();
+
+const documento = document.getElementById('documento')
+const indicacion = document.getElementById('indicacion')
+
+const boton = document.getElementById('boton')
+
+const tabla = document.getElementById('tablaListada')
+
+boton.addEventListener('click', crear)
+
